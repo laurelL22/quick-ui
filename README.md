f09a7ceef571cf5e4e00b448fbc3d92102f8baae
laurelL22 <lihuan_1212@163.com>
2020-01-17 15:19:47 +0800

    OverView：update UI & utils

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # quick-ui
 快应用UI组件
 
-### Contents
+### UI Components
 - icon
 - modal
+- imageBox
+
+### utils
+- nativeApi // 快应用API promise化
+    - eg: `nativeApi('prompt.showToast', {message: 'message'})`
---
+++ utils/native.js
@@ -0,0 +1,33 @@
+
+const prompt = require('@system.prompt');
+
+const router = require('@system.router');
+
+const media = require('@system.media');
+
+const request = require('@system.request');
+
+const image = require('@system.image');
+
+const NATIVE = {
+    prompt,
+    router,
+    request,
+    media,
+    image
+    // ... 需要的接口自行添加
+}
+
+
+function nativeApi(actions, option) {
+    const [action, name] = actions.split('.');
+  
+    return new Promise((resolve, reject) => 
+        NATIVE[action][name](Object.assign({}, option, {
+            success: resolve,
+            fail: reject
+        }))
+    )
+}
+
+export default nativeApi;
